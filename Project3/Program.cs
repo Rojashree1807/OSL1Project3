1e90aecf1550444303da3503aefd305727699f2c
Rojashree1807 <rojarojashree1807@gmail.com>
2023-11-28 16:49:45 +0530

    Initial Commit

---
+++ Project3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Project3
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+    internal class Program
+    {
+        static List<Student> students = new List<Student>();
+        static void Main(string[] args)
+        {
+            LoadData();
+            Console.WriteLine("\nStudents Data before Sorting:");
+            Display(students);
+            students.Sort((s1, s2) => string.Compare(s1.Name, s2.Name,
+           StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("\nStudent Data After Sorting \n");
+            DisplaySortedData();
+            Console.Write("\nEnter a student's name to search: ");
+            string searchName = Console.ReadLine();
+            SearchByName(searchName);
+            Console.ReadKey();
+        }
+        static void LoadData()
+        {
+            try
+            {
+                string filePath = "C:\\Users\\rojar\\OneDrive\\Desktop\\Simpli learn\\OSL1\\Project3\\Project3\\StudentData1.txt";
+
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    foreach (string line in lines)
+                    {
+                        string[] data = line.Split(',');
+                        if (data.Length == 2)
+                        {
+                            string name = data[0].Trim();
+                            string studentClass = data[1].Trim();
+                            students.Add(new Student { Name = name, Class = studentClass });
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File not found: student.txt");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in loading data from the file!!!! " + ex.Message);
+            }
+        }
+        static void Display(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, \t\t\t\tClass: {student.Class}");
+            }
+        }
+        static void DisplaySortedData()
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, \t\t\t\tClass: {student.Class}");
+            }
+        }
+        static void SearchByName(string searchName)
+        {
+            bool found = false;
+            foreach (var student in students)
+            {
+                if (student.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"\nStudent Found: Name: {student.Name}, Class: {student.Class}");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("\nStudent not found!!!");
+            }
+        }
+    }
+}
